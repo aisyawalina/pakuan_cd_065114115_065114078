0dc2fc50c819fb1f371975ac35eade2321a7f332
aquarius <aquarius@laila-PC>
2016-12-07 21:29:49 -0800

    luas dan keliling segitiga

--- src/bangun_segitiga/Bangun_segitiga.java
+++ src/bangun_segitiga/Bangun_segitiga.java
@@ -22,22 +22,28 @@ public class Bangun_segitiga {
     double a;
     double t;
     double luas;
+    double s;
+    double keliling;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
          Scanner input = new Scanner(System.in);
-         double a,t,luas; 
+         double a,t,luas,s,keliling; 
          
          System.out.print ("Input Alas : ");
          a = input.nextDouble();
          System.out.print ("Input Tinggi : ");
          t = input.nextDouble();
-         
+         System.out.print ("Input Sisi : ");
+         s = input.nextDouble();
          luas = (a*t)/2;
          
         System.out.println ("Luas Segitiga : " + luas); 
+        
+        keliling = s+s+s; 
+        System.out.println ("Keliling Segitiga : " + keliling); 
          
     }
     
