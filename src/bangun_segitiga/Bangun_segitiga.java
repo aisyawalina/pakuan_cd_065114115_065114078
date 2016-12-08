c1ae78ff4a5c99bf5244b532d3a66eadd40e4655
Win 7 <Win 7@Aisya>
2016-12-08 12:23:41 +0700

    luas segitiga

---
+++ src/bangun_segitiga/Bangun_segitiga.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bangun_segitiga;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Win 7
+ */
+public class Bangun_segitiga {
+
+/**
+ *
+ * @author Win 7
+ */
+
+    double a;
+    double t;
+    double luas;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner input = new Scanner(System.in);
+         double a,t,luas; 
+         
+         System.out.print ("Input Alas : ");
+         a = input.nextDouble();
+         System.out.print ("Input Tinggi : ");
+         t = input.nextDouble();
+         
+         luas = (a*t)/2;
+         
+        System.out.println ("Luas Segitiga : " + luas); 
+         
+    }
+    
+}
